e1d791c19ab30a8601748b448eb8bf82c0463b6e
Arnas <arnasdickus@protonmail.com>
2020-10-14 13:53:19 +0300

    added basic navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import HomePage from "./pages/homepage/homepage.component";
 import './App.scss';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const HatsPage = () => (
+    <div>
+        <h1>Hats page</h1>
+    </div>
+)
 
 function App() {
   return (
     <div>
-        <HomePage />
+        <Router>
+            <Switch>
+                <Route exact={true} path="/" component={HomePage} />
+                <Route path="/shop/hats" component={HatsPage} />
+            </Switch>
+        </Router>
     </div>
   );
 }
