c7155fafafb54ab5c42e78d944908f749ac272b8
Arnas <arnasdickus@protonmail.com>
2020-10-15 11:18:21 +0300

    created header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 import './App.scss';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -8,11 +9,11 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div>
+        <Header/>
         <Router>
             <Switch>
                 <Route exact={true} path="/" component={HomePage} />
                 <Route path="/shop" component={ShopPage} />
-                {/*<Route path="/shop/hats" component={HatsPage} />*/}
             </Switch>
         </Router>
     </div>
