1bfeb318a8a92eeb322c36136a16a7afa8cdbfb6
Arnas Dickus <arnasdickus@protonmail.com>
2020-10-26 14:16:30 +0200

    styling issue fix

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -5,15 +5,15 @@ import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = ({ cartItems }) => (
-    <div className="cart-dropdown">
-        <div className="cart-items">
-            {
-                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
-            }
-            <CustomButton>GO TO CHECKOUT</CustomButton>
+    <div className='cart-dropdown'>
+        <div className='cart-items'>
+            {cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))}
         </div>
+        <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
-)
+);
 
 const mapStateToProps = ({cart: {cartItems}}) => ({
     cartItems
