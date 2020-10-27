8692fc107d80117f3ff13e859dbf6b0069baff19
Arnas Dickus <arnasdickus@protonmail.com>
2020-10-27 13:01:45 +0200

    refractor code with create structure selector

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-d
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions'
-
-
+import { selectCurrentUser } from "./redux/user/user.selector";
+import { createStructuredSelector } from "reselect";
 
 class App extends React.Component {
     constructor(props) {
@@ -61,8 +61,8 @@ class App extends React.Component {
     }
 }
 
-const mapStateToProps = ({user}) => ({
-    currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
